7cedf89df3a5e6a26dfb2f449f2884c1af7424c6
Ádám Knapecz <knapecz.adam@gmail.com>
2023-11-11 16:41:07 +0100

    Fix timer's update: move logic to one scope deeper

--- Library/src/Timer.cpp
+++ Library/src/Timer.cpp
@@ -14,29 +14,6 @@ namespace dae
         m_SecondsPerCount = 1.0f / static_cast<float>(countsPerSecond);
     }
 
-    void Timer::StartBenchmark(int numFrames)
-    {
-        if (m_BenchmarkActive)
-        {
-            std::cout << "(Benchmark already running)";
-            return;
-        }
-
-        m_BenchmarkActive = true;
-
-        m_BenchmarkAvg = 0.f;
-        m_BenchmarkHigh = FLT_MIN;
-        m_BenchmarkLow = FLT_MAX;
-
-        m_BenchmarkFrames = numFrames;
-        m_BenchmarkCurrFrame = 0;
-
-        m_Benchmarks.clear();
-        m_Benchmarks.resize(m_BenchmarkFrames);
-
-        std::cout << "**BENCHMARK STARTED**\n";
-    }
-
     void Timer::Reset()
     {
         const uint64_t currentTime = SDL_GetPerformanceCounter();
@@ -61,6 +38,29 @@ namespace dae
             m_StopTime = 0;
             m_IsStopped = false;
         }
+    }
+
+    void Timer::StartBenchmark(int numFrames)
+    {
+        if (m_BenchmarkActive)
+        {
+            std::cout << "(Benchmark already running)";
+            return;
+        }
+
+        m_BenchmarkActive = true;
+
+        m_BenchmarkAvg = 0.f;
+        m_BenchmarkHigh = FLT_MIN;
+        m_BenchmarkLow = FLT_MAX;
+
+        m_BenchmarkFrames = numFrames;
+        m_BenchmarkCurrFrame = 0;
+
+        m_Benchmarks.clear();
+        m_Benchmarks.resize(m_BenchmarkFrames);
+
+        std::cout << "**BENCHMARK STARTED**\n";
     }
 
     void Timer::Update()
@@ -98,35 +98,35 @@ namespace dae
             m_FPS = m_FPSCount;
             m_FPSCount = 0;
             m_FPSTimer = 0.0f;
-        }
 
-        if (m_BenchmarkActive)
-        {
-            m_Benchmarks[m_BenchmarkCurrFrame] = m_dFPS;
-
-            m_BenchmarkLow = std::min(m_BenchmarkLow, m_dFPS);
-            m_BenchmarkHigh = std::max(m_BenchmarkHigh, m_dFPS);
-
-            ++m_BenchmarkCurrFrame;
-            if (m_BenchmarkCurrFrame >= m_BenchmarkFrames)
+            if (m_BenchmarkActive)
             {
-                m_BenchmarkActive = false;
-                m_BenchmarkAvg = std::accumulate(m_Benchmarks.begin(), m_Benchmarks.end(), 0.f) / float(
-                    m_BenchmarkFrames);
+                m_Benchmarks[m_BenchmarkCurrFrame] = m_dFPS;
 
-                //print
-                std::cout << "**BENCHMARK FINISHED**\n";
-                std::cout << ">> HIGH = " << m_BenchmarkHigh << std::endl;
-                std::cout << ">> LOW = " << m_BenchmarkLow << std::endl;
-                std::cout << ">> AVG = " << m_BenchmarkAvg << std::endl;
+                m_BenchmarkLow = std::min(m_BenchmarkLow, m_dFPS);
+                m_BenchmarkHigh = std::max(m_BenchmarkHigh, m_dFPS);
 
-                //file save
-                std::ofstream fileStream("benchmark.txt");
-                fileStream << "FRAMES = " << m_BenchmarkCurrFrame << std::endl;
-                fileStream << "HIGH = " << m_BenchmarkHigh << std::endl;
-                fileStream << "LOW = " << m_BenchmarkLow << std::endl;
-                fileStream << "AVG = " << m_BenchmarkAvg << std::endl;
-                fileStream.close();
+                ++m_BenchmarkCurrFrame;
+                if (m_BenchmarkCurrFrame >= m_BenchmarkFrames)
+                {
+                    m_BenchmarkActive = false;
+                    m_BenchmarkAvg = std::accumulate(m_Benchmarks.begin(), m_Benchmarks.end(), 0.f) / float(
+                        m_BenchmarkFrames);
+
+                    //print
+                    std::cout << "**BENCHMARK FINISHED**\n";
+                    std::cout << ">> HIGH = " << m_BenchmarkHigh << std::endl;
+                    std::cout << ">> LOW = " << m_BenchmarkLow << std::endl;
+                    std::cout << ">> AVG = " << m_BenchmarkAvg << std::endl;
+
+                    //file save
+                    std::ofstream fileStream("benchmark.txt");
+                    fileStream << "FRAMES = " << m_BenchmarkCurrFrame << std::endl;
+                    fileStream << "HIGH = " << m_BenchmarkHigh << std::endl;
+                    fileStream << "LOW = " << m_BenchmarkLow << std::endl;
+                    fileStream << "AVG = " << m_BenchmarkAvg << std::endl;
+                    fileStream.close();
+                }
             }
         }
     }
--- Library/src/Timer.h
+++ Library/src/Timer.h
@@ -16,7 +16,7 @@ namespace dae
         Timer(Timer&&) noexcept = delete;
         Timer& operator=(const Timer&) = delete;
         Timer& operator=(Timer&&) noexcept = delete;
-        
+
         void StartBenchmark(int numFrames = 10);
 
         void Reset();
@@ -24,32 +24,32 @@ namespace dae
         void Update();
         void Stop();
 
-        uint32_t GetFPS() const { return m_FPS; };
-        float GetdFPS() const { return m_dFPS; };
-        float GetElapsed() const { return m_ElapsedTime; };
-        float GetTotal() const { return m_TotalTime; };
-        bool IsRunning() const { return !m_IsStopped; };
+        uint32_t GetFPS() const { return m_FPS; }
+        float GetdFPS() const { return m_dFPS; }
+        float GetElapsed() const { return m_ElapsedTime; }
+        float GetTotal() const { return m_TotalTime; }
+        bool IsRunning() const { return !m_IsStopped; }
 
     private:
-        uint64_t m_BaseTime = 0;
-        uint64_t m_PausedTime = 0;
-        uint64_t m_StopTime = 0;
-        uint64_t m_PreviousTime = 0;
-        uint64_t m_CurrentTime = 0;
+        uint64_t m_BaseTime     {0};
+        uint64_t m_PausedTime   {0};
+        uint64_t m_StopTime     {0};
+        uint64_t m_PreviousTime {0};
+        uint64_t m_CurrentTime  {0};
 
-        uint32_t m_FPS = 0;
-        float m_dFPS = 0.0f;
-        uint32_t m_FPSCount = 0;
+        uint32_t m_FPS      {0};
+        float    m_dFPS     {0.0f};
+        uint32_t m_FPSCount {0};
 
-        float m_TotalTime = 0.0f;
-        float m_ElapsedTime = 0.0f;
-        float m_SecondsPerCount = 0.0f;
-        float m_ElapsedUpperBound = 0.03f;
-        float m_FPSTimer = 0.0f;
+        float m_TotalTime         {0.0f};
+        float m_ElapsedTime       {0.0f};
+        float m_SecondsPerCount   {0.0f};
+        float m_ElapsedUpperBound {0.03f};
+        float m_FPSTimer          {0.0f};
 
-        bool m_IsStopped = true;
-        bool m_ForceElapsedUpperBound = false;
-        
+        bool m_IsStopped              {true};
+        bool m_ForceElapsedUpperBound {false};
+
         bool               m_BenchmarkActive    {false};
         float              m_BenchmarkHigh      {0.0f};
         float              m_BenchmarkLow       {0.0f};
